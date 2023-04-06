2b995f910fb0bdaf50ed15f0674360df9583e212
Th-Guasti <thaslesouza@gmail.com>
2023-04-06 19:17:27 -0300

    avançando codigo para pasta componentes

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-import logo from './imagens/logo.svg';
+import Logo from './componentes/logo';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Alura Books</strong></p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
