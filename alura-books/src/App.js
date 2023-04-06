9d5070b34a71cbc9ccae2dd8b62cf1497d4387d7
Th-Guasti <thaslesouza@gmail.com>
2023-04-06 16:11:16 -0300

    linha de código mudada no js para fazer teste e imagens add

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Alura
         </p>
         <a
           className="App-link"
