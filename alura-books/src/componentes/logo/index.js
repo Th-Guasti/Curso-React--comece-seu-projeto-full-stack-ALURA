6bf29f3eaf72bc3ad8b2327c198f226b4afe30ca
Th-Guasti <thaslesouza@gmail.com>
2023-04-28 18:29:45 -0300

    estilizações na logo, no header e no background da pagina

--- alura-books/src/componentes/logo/index.js
+++ alura-books/src/componentes/logo/index.js
@@ -4,7 +4,12 @@ import './estilo.css'
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='logo'></img>
+            <img 
+            src={logo} 
+            alt='logo' 
+            className='logo-img'>
+                
+            </img>
             <p><strong>Alura</strong>Books</p>
         </div>
     )
