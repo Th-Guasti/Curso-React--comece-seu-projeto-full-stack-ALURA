9f3b158301fac37175f600db0b568a0c5234ff36
Th-Guasti <thaslesouza@gmail.com>
2023-05-09 16:18:27 -0300

    componentização do header finalizado

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,16 +1,10 @@
 import './App.css'
-import Logo from './componentes/logo'
-import OpcoesHeader from './componentes/opcoesHeader'
-import IconesHeader from './componentes/iconesHeader'
+import Header from './componentes/Header';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-        <IconesHeader></IconesHeader>
-      </header>
+      <Header/>
     </div>
   );
 }
