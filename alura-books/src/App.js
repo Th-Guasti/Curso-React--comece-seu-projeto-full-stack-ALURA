2384c4bf8ec4b7d6a5779b8b51264b822ff1acfb
Th-Guasti <thaslesouza@gmail.com>
2023-05-09 16:03:41 -0300

    componentização iniciada

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,26 +1,15 @@
-import './App.css';
-import Logo from './componentes/logo';
-import perfil from './imagens/perfil.svg';
-import sacola from './imagens/sacola.svg';
-
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
-const icones = [perfil, sacola];
+import './App.css'
+import Logo from './componentes/logo'
+import OpcoesHeader from './componentes/opcoesHeader'
+import IconesHeader from './componentes/iconesHeader'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
-        <ul className='opcoes'>
-          { textoOpcoes.map((texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-          ))}
-        </ul>
-        <ul className='icones'>
-          { icones.map ((icone) => (
-            <li className='icone'><img src={icone}></img></li>
-          ))}
-        </ul>
+        <OpcoesHeader></OpcoesHeader>
+        <IconesHeader></IconesHeader>
       </header>
     </div>
   );
---
+++ alura-books/src/componentes/iconesHeader/index.js
@@ -0,0 +1,17 @@
+import perfil from '../../imagens/perfil.svg';
+import sacola from '../../imagens/sacola.svg';
+import './estilo.css'
+
+const icones = [perfil, sacola];
+
+function IconesHeader() {
+    return (
+        <ul className='icones'>
+            {icones.map((icone) => (
+                <li className='icone'><img src={icone}></img></li>
+            ))}
+        </ul>
+    )
+}
+
+export default IconesHeader
