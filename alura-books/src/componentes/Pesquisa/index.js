2e93a02766574ef941b35ffe826c8f57fb70b16f
Th-Guasti <thaslesouza@gmail.com>
2023-06-01 15:43:06 -0300

    Pesquisa agora aparece na tela com nome e img

--- alura-books/src/componentes/Pesquisa/index.js
+++ alura-books/src/componentes/Pesquisa/index.js
@@ -22,10 +22,25 @@ const Subtitulo = styled.h3`
     font-weight: 500;
     margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-    console.log(livrosPesquisados)
 
     return (
         <PesquisaContainer>
@@ -39,6 +54,12 @@ function Pesquisa() {
                     setLivrosPesquisados(resultadoPesquisa);
                 }            
             }/>
+            { livrosPesquisados.map(livro => (
+                <Resultado>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src}/>
+                </Resultado>
+            )) }
         </PesquisaContainer>
     )
 }
